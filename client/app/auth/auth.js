47b202f88260673c53f1de4bbc984efb30e76f62
mihyar-30614 <eng.mihyear@gmail.com>
2016-07-18 09:49:28 +0300

    adding authorization files and applying it

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,7 +1,4 @@
-// do not tamper with this code in here, study it, but do not touch
-// this Auth controller is responsible for our client side authentication
-// in our signup/signin forms using the injected Auth service
-angular.module('shortly.auth', [])
+angular.module('Khitwa.auth', [])
 
 .controller('AuthController', function ($scope, $window, $location, Auth) {
   $scope.user = {};
@@ -9,8 +6,8 @@ angular.module('shortly.auth', [])
   $scope.signin = function () {
     Auth.signin($scope.user)
       .then(function (token) {
-        $window.localStorage.setItem('com.shortly', token);
-        $location.path('/links');
+        $window.localStorage.setItem('com.Khitwa', token);
+        $location.path('/events');
       })
       .catch(function (error) {
         console.error(error);
@@ -20,8 +17,8 @@ angular.module('shortly.auth', [])
   $scope.signup = function () {
     Auth.signup($scope.user)
       .then(function (token) {
-        $window.localStorage.setItem('com.shortly', token);
-        $location.path('/links');
+        $window.localStorage.setItem('com.Khitwa', token);
+        $location.path('/events');
       })
       .catch(function (error) {
         console.error(error);
