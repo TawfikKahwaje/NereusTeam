6c20d628e666fe63a8c04704306db859f1e91a34
mihyar-30614 <eng.mihyear@gmail.com>
2016-07-22 14:13:02 +0300

    events functions

--- client/app/events/events.js
+++ client/app/events/events.js
@@ -1,4 +1,11 @@
 angular.module('Khitwa.events', [])
-.controller('EventsCtrl', function ($scope) {
-	
+.controller('EventsCtrl', function ($scope, Events) {
+	$scope.data={};
+	Events.getEvents()
+	.then(function(events){
+		$scope.data.events = events;
+	})
+	.catch(function(error){
+		console.error(error)
+	})
 });
