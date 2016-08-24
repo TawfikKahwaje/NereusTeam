d8fd8422bc53664a3fc3f361b18570932102fdf9
Tawfik Kahwaje <tawfik.kahwaje@gmail.com>
2016-08-24 18:53:00 +0300

    Add prMONGODB_URI

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var user=require('./users/userModel.js');
 var user=require('./events/eventModel.js');
 
 var app = express();
+var server   = require('http').createServer(app);
 
 var mongoURI =  process.env.MONGODB_URI || 'mongodb://localhost/Khitwa';
 
@@ -16,8 +17,9 @@ require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
 // start listening to requests on port 8000
-app.listen(8000);
-console.log('listen to ');
+server.listen(port, function(){
+	console.log('Server now listening on port ',port )
+});
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
